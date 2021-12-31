83dcca9ceb2ac690e6bc2d5f98a376525166c8b2
Joel Auccapuclla <auccapuclla2013@gmail.com>
2021-12-31 11:41:16 +0800

    add the query handler

--- index.py
+++ index.py
@@ -6,14 +6,24 @@ class basicRequestHandler(tornado.web.RequestHandler):
         self.write("Hello, World this is a python command executed from the backend.")
 
 
+class queryParamRequestHandler(tornado.web.RequestHandler):
+    def get(self):
+       num = self.get_argument("num")
+       if (num.isdigit()):
+          r = "odd" if int(num) % 2 else "even"
+          self.write(f"The integer {num} is {r}")
+       else:
+          self.write(f"{num} is not a valid integer.")        
+
 class listRequestHandler(tornado.web.RequestHandler):
     def get(self):
         self.render("index.html")
 
 if __name__ == "__main__":
     app = tornado.web.Application([
-        (r"/", basicRequestHandler),
-        (r"/animal", listRequestHandler)
+        (r"/", basicRequestHandler), #r Rest point
+        (r"/animal", listRequestHandler),
+        (r"/isEven", queryParamRequestHandler) #to query we need to use route "/isEven?num=4"
     ])
     
     port = 8882
